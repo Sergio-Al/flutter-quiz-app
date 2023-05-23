1af5bae7b954ec01e3059b197541a3bba797bf00
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2023-05-22 21:36:12 -0400

    Mutating values in memory
    we can use build-in functions that will help us to reach our objectives.

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -35,7 +35,7 @@ class _QuestionScreenState extends State<QuestionScreen> {
             const SizedBox(
               height: 30,
             ),
-            ...currentQuestion.answers.map((item) {
+            ...currentQuestion.getShuffledAnswers().map((item) {
               return AnswerButton(item, () {});
             }),
           ],
